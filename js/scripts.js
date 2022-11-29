f9d50fbc3edd3deab150ecf2b1ee6a9608ac9500
Zaid <freeze4allems@gmail.com>
2022-11-29 00:07:57 -0400

    Almost done with Story Section

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,13 @@
 
 // HEADER SCROLL FEATURE
 document.addEventListener('scroll', (e) => {
+    let url = window.location.origin
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         document.getElementById("header").style.backgroundColor = "white";
+        document.querySelector(".header_logo").src = `${url}/images/icons/aV-header-main-site.png`
       } else {
         document.getElementById("header").style.backgroundColor = "transparent";
+        document.querySelector(".header_logo").src = `${url}/images/icons/logo.png`
       }
 });
 
@@ -16,7 +19,12 @@ document.addEventListener('scroll', (e) => {
  ------------------------- HOMPAGE -----------------------------
  **/
 
+// HOMEPAGE VIDEO FEAUTURE
+var video=document.getElementById("myVideo") ;   
 
+video.addEventListener('click', function(e){
+  video.muted = !video.muted;
+});
 
 //  HOMEPAGE BANNER GSAP
 let bannerHeader = document.querySelector(".home-page-banner__text-1");
@@ -26,7 +34,7 @@ gsap.fromTo([bannerHeader, bannerText], {
   opacity: 0,
 }, {
   y: 0,
-  toggleActions: "restart pause reverse restart",
+  // toggleActions: "play pause resume reset",
   opacity: 1,
   duration: 2
 });
@@ -69,11 +77,7 @@ zaid_about.fromTo([aboutHeader, aboutText], {
     opacity: 1,
     // text: "ABOUT",
     scale: 1,
-}).to( [aboutHeader, aboutText, aboutLine], {
-  delay: 1,
-  opacity: 0
 })
-
 
 
 
@@ -81,9 +85,9 @@ zaid_about.fromTo([aboutHeader, aboutText], {
 
 // HOMEPAGE IMAGE GRID GSAP TIMELINE
 let zaid_grid = gsap.timeline({
+  // toggleActions: "restart pause resume pause",
     scrollTrigger: {
         trigger: "#image-grid",
-        toggleActions: "restart pause play restart",
         start: "top bottom+=350px",
         scrub: true,
         // pin: true,
@@ -97,7 +101,7 @@ let gridImg3 = document.querySelector(".image-grid__img-3");
 
   zaid_grid.fromTo([gridImg1, gridImg2, gridImg3], {
     scrollTrigger: gridImg2,
-    scale: 1.15,
+    scale: 1.3,
 }, {
     scale: 1,
     ease: "slow(0.7, 0.7, false)",
@@ -142,7 +146,6 @@ zaid_preorder.fromTo([preorderHeader, preorderText], {
 
 },{
   opacity: 1,
-  // text: "ABOUT",
   scale: 1,
 }).to( [preorderHeader, preorderText, preorderLine],{
   delay: 1,
@@ -154,32 +157,28 @@ zaid_preorder.fromTo([preorderHeader, preorderText], {
 // PREORDER HORIZONTAL TITLE
 gsap.fromTo(".pre-order__text", {
   // opacity: 0,
-  scale: 0.5,
+  scale: 1.5,
+  x: '-110vw',
 
 }, {
   scrollTrigger: {
-    trigger: ".pre-order__text",
+    trigger: ".hor-1",
     scrub: true,
+    pin: true,
+    start: "top top",
+    // end: "+=10%",
+    anticipatePin: 1,
   },
+  x: 0,
   scale: 1,
 })
 
-// All the Images
-// let horizontalImg = gsap.utils.toArray(".scroll-container-body img");
-// gsap.fromTo(".scroll-container-body img", {
-//   opacity: 0,
-// }, {
-//   scrollTrigger: {
-//     trigger: ".scroll-container-body img",
-//     scrub: true,
-//   },
-//   opacity: 1
-// })
-
 // HORIZONTAL SCROLL
+
+let zaid_hor = gsap.timeline()
 let sections = gsap.utils.toArray(".panel");
  
-gsap.to(sections, {
+zaid_hor.to(sections, {
     xPercent: -100 * (sections.length - 1),
     ease: "none",
     scrollTrigger: {
@@ -190,6 +189,60 @@ gsap.to(sections, {
       // base vertical scrolling on how wide the container is so it feels more natural.
       end: "+=3500",
     }
+})
+
+  
+  // THE GIRL IN THE MIDDLE
+  gsap.utils.toArray(".Zpanel").forEach((panel, i) => {
+    ScrollTrigger.create({
+      trigger: panel,
+      start: "top top", 
+      pin: true, 
+      pinSpacing: false 
+    });
   });
+  
+
+
+  // THE STORY
+  let zaid_story = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#story",
+        scrub: true,
+        pin: true,
+        start: "top top-=15px",
+        end: "+=150%",
+        anticipatePin: 1,
+      },
+
+  });
+
+//   The about Line
+zaid_story.fromTo(".story-line", {
+    scaleX: 0, 
+}, {
+    scaleX: 1,
+    transformOrigin: "right center", 
+    ease: "none"
+})
+
+//   The about heading text
+let storyHeader = document.querySelector(".story__header");
+let storyBgImage = document.querySelector(".story-bg-img");
+let storyText = document.querySelector(".story-text");
+let storyImg2 = document.querySelector(".the-story-second-img");
+zaid_story.fromTo([storyHeader], {
+    opacity: 0,
+    scale: 1.5,
+
+},{
+    opacity: 1,
+    scale: 1.5,
+}).fromTo([storyBgImage, storyText, storyImg2], {
+  opacity: 0
+}, {
+  opacity: 1,
+  scale: 1,
+})
 
   
